b900e6860d2243bd86fe0d381ea8e5697e0c2e7c
Adam Anderson Yuniarto <adam.anderson.ft21@mail.umy.ac.id>
2023-12-28 01:40:21 +0700

    add override suspend fun delete and insert

--- app/src/main/java/com/example/consumeapi/repository/KontakRepository.kt
+++ app/src/main/java/com/example/consumeapi/repository/KontakRepository.kt
@@ -5,6 +5,8 @@ import com.example.consumeapi.service_api.KontakService
 
 interface KontakRepository {
     suspend fun getKontak(): List<Kontak>
+    suspend fun deleteKontak(id: Int): Boolean
+    suspend fun insertKontak(kontak: Kontak): Boolean
 }
 
 class NetworkKontakRepository(
@@ -12,4 +14,19 @@ class NetworkKontakRepository(
 ) : KontakRepository {
 
     override suspend fun getKontak(): List<Kontak> = kontakApiService.getKontak()
+    override suspend fun deleteKontak(id: Int): Boolean {
+        // Implementasi penghapusan kontak melalui kontakApiService dengan menggunakan id
+        val response = kontakApiService.deleteKontak(id)
+
+        // Mengembalikan nilai true jika penghapusan berhasil, false sebaliknya
+        return response.isSuccessful
+    }
+
+    override suspend fun insertKontak(kontak: Kontak): Boolean {
+        // Implementasi penambahan kontak melalui kontakApiService
+        val response = kontakApiService.insertKontak(kontak)
+
+        // Mengembalikan nilai true jika penambahan berhasil, false sebaliknya
+        return response.isSuccessful
+    }
 }
