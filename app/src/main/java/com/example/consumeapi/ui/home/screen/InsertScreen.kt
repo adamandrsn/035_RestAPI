b071574b2ea1f4f56eef2238b05568ff9a307e43
Adam Anderson Yuniarto <adam.anderson.ft21@mail.umy.ac.id>
2023-12-28 01:53:03 +0700

    add InsertScreen

--- app/src/main/java/com/example/consumeapi/ui/home/screen/InsertScreen.kt
+++ app/src/main/java/com/example/consumeapi/ui/home/screen/InsertScreen.kt
@@ -1,59 +1,261 @@
 package com.example.consumeapi.ui.home.screen
 
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.example.consumeapi.model.Kontak
-import com.example.consumeapi.repository.KontakRepository
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.consumeapi.navigation.DestinasiNavigasi
+import com.example.consumeapi.ui.PenyediaViewModel
+import com.example.consumeapi.ui.TopAppBarKontak
 import kotlinx.coroutines.launch
 
-class InsertViewModel (private val kontakRepository: KontakRepository) : ViewModel() {
+object DestinasiEntry : DestinasiNavigasi {
 
-    var insertKontakState by mutableStateOf(InsertUiState())
-        private set
+    override val route = "item_entry"
 
-    fun updateInsertKontakState(insertUiEvent: InsertUiEvent){
-        insertKontakState = InsertUiState(insertUiEvent = insertUiEvent)
-    }
+    override val titleRes = "Entry Siswa"
 
-    suspend fun insertKontak(){
-        viewModelScope.launch {
-            try {
-                kontakRepository.insertKontak(insertKontakState.insertUiEvent.toKontak())
-            }catch (e: Exception){
-                e.printStackTrace()
-            }
-        }
-    }
 }
 
-data class InsertUiState(
-    val insertUiEvent: InsertUiEvent = InsertUiEvent(),
-)
 
-data class InsertUiEvent(
-    val id: Int = 0,
-    val nama: String = "",
-    val email: String = "",
-    val nohp: String = "",
-)
 
-fun InsertUiEvent.toKontak(): Kontak = Kontak(
-    id = id,
-    nama = nama,
-    email = email,
-    nohp = nohp
-)
 
-fun Kontak.toUiStateKontak (): InsertUiState = InsertUiState(
-    insertUiEvent = toInsertUiEvent(),
-)
+@OptIn(ExperimentalMaterial3Api::class)
 
-fun Kontak.toInsertUiEvent(): InsertUiEvent = InsertUiEvent(
-    id = id,
-    nama = nama,
-    email = email,
-    nohp = nohp
-)
+@Composable
+
+fun EntryKontakScreen(
+
+    navigateBack: () -> Unit,
+
+    modifier: Modifier = Modifier,
+
+    viewModel: InsertViewModel = viewModel(factory = PenyediaViewModel.Factory),
+
+    ) {
+
+    val coroutineScope = rememberCoroutineScope()
+
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    Scaffold(
+
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+
+        topBar = {
+
+            TopAppBarKontak(
+
+                title = DestinasiEntry.titleRes,
+
+                canNavigateBack = true,
+
+                scrollBehavior = scrollBehavior,
+
+                navigateUp = navigateBack
+
+            )
+
+        }
+
+    ) { innerPadding ->
+
+        EntryKontakBody(
+
+            insertUiState = viewModel.insertKontakState,
+
+            onSiswaValueChange = viewModel::updateInsertKontakState,
+
+            onSaveClick = {
+
+                coroutineScope.launch {
+
+                    viewModel.insertKontak()
+
+                    navigateBack()
+
+                }
+
+            },
+
+            modifier = Modifier
+
+                .padding(innerPadding)
+
+                .verticalScroll(rememberScrollState())
+
+                .fillMaxWidth()
+
+        )
+
+
+
+
+    }
+
+}
+
+
+
+
+@Composable
+
+fun EntryKontakBody(
+
+    insertUiState: InsertUiState,
+
+    onSiswaValueChange: (InsertUiEvent) -> Unit,
+
+    onSaveClick: () -> Unit,
+
+    modifier: Modifier = Modifier
+
+) {
+
+    Column(
+
+        verticalArrangement = Arrangement.spacedBy(18.dp),
+
+        modifier = modifier.padding(12.dp)
+
+    ) {
+
+        FormInputSiswa(
+
+            insertUiEvent = insertUiState.insertUiEvent,
+
+            onValueChange = onSiswaValueChange,
+
+            modifier = Modifier.fillMaxWidth()
+
+        )
+
+        Button(
+
+            onClick = onSaveClick,
+
+            shape = MaterialTheme.shapes.small,
+
+            modifier = Modifier.fillMaxWidth()
+
+        ) {
+
+            Text(text = "Simpan")
+
+        }
+
+    }
+
+}
+
+
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+
+@Composable
+
+fun FormInputSiswa(
+
+    insertUiEvent: InsertUiEvent,
+
+    modifier: Modifier = Modifier,
+
+    onValueChange: (InsertUiEvent) -> Unit = {},
+
+    enabled: Boolean = true
+
+) {
+
+    Column(
+
+        modifier = modifier,
+
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+
+    ) {
+
+        OutlinedTextField(
+
+            value = insertUiEvent.nama,
+
+            onValueChange = { onValueChange(insertUiEvent.copy(nama = it)) },
+
+            label = { Text("Nama") },
+
+            modifier = Modifier.fillMaxWidth(),
+
+            enabled = enabled,
+
+            singleLine = true
+
+        )
+
+        OutlinedTextField(
+
+            value = insertUiEvent.email,
+
+            onValueChange = { onValueChange(insertUiEvent.copy(email = it)) },
+
+            label = { Text("Email") },
+
+            modifier = Modifier.fillMaxWidth(),
+
+            enabled = enabled,
+
+            singleLine = true
+
+        )
+
+        OutlinedTextField(
+
+            value = insertUiEvent.nohp,
+
+            onValueChange = { onValueChange(insertUiEvent.copy(nohp = it)) },
+
+            label = { Text("No HP") },
+
+            modifier = Modifier.fillMaxWidth(),
+
+            enabled = enabled,
+
+            singleLine = true
+
+        )
+
+
+
+
+        if (enabled) {
+
+            Text(text = "Isi semua data", modifier = Modifier.padding(12.dp))
+
+        }
+
+        Divider(
+
+            thickness = 8.dp,
+
+            modifier = Modifier.padding(12.dp)
+
+        )
+
+    }
+
+}
