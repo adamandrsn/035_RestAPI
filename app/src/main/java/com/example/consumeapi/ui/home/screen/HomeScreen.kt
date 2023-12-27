53ef68e85bb66e23deb0756dfede9765da3654c4
Adam Anderson Yuniarto <adam.anderson.ft21@mail.umy.ac.id>
2023-12-28 01:34:36 +0700

    add function HomeScreen

--- app/src/main/java/com/example/consumeapi/ui/home/screen/HomeScreen.kt
+++ app/src/main/java/com/example/consumeapi/ui/home/screen/HomeScreen.kt
@@ -1,2 +1,22 @@
 package com.example.consumeapi.ui.home.screen
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.consumeapi.navigation.DestinasiNavigasi
+import com.example.consumeapi.ui.PenyediaViewModel
+import com.example.consumeapi.ui.home.viewmodel.HomeViewModel
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = "Kontak"
+}
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(
+    navigateToItemEntry: () -> Unit,
+    modifier: Modifier = Modifier,
+    onDetailClick: (Int) -> Unit = {},
+    viewModel: HomeViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {}
